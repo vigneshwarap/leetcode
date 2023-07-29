58ccc03fa92c62d861d511f6b3e374e3f6c26f9a
vigneshwara palanisamy <vigneshwarap48@gmail.com>
2023-07-29 18:31:04 +0530

    1732. Find the Highest Altitude

---
+++ src/main/java/Find_the_Highest_Altitude_1732.java
@@ -0,0 +1,15 @@
+public class Find_the_Highest_Altitude_1732 {
+    public static int largestAltitude(int[] gain) {
+        int current = 0, max=0;
+        for (int j : gain) {
+            current += j;
+            max = Math.max(max, current);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] gain = {-5,1,5,0,-7};
+        System.out.println(largestAltitude(gain));
+    }
+}
