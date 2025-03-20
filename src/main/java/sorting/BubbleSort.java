a835493672bdbd9c9bbdaae82277dc080708ec9d
vigneshwara palanisamy <vigneshwarap48@gmail.com>
2025-03-20 06:21:08 +0530

    Selection sort, Bubble sort, Insertion sort

---
+++ src/main/java/sorting/BubbleSort.java
@@ -0,0 +1,15 @@
+package sorting;
+
+public class BubbleSort {
+    public static void bubbleSort(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j ++) {
+                if (arr[j] > arr[j+1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                }
+            }
+        }
+    }
+}
