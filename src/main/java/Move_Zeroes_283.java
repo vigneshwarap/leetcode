d81b1004bc3db3b1a0d417020c3e4137985fdbde
vigneshwara palanisamy <vigneshwarap48@gmail.com>
2025-03-23 22:52:57 +0530

    283. Move Zeroes

--- src/main/java/Move_Zeroes_283.java
+++ src/main/java/Move_Zeroes_283.java
@@ -2,7 +2,29 @@ import java.util.Arrays;
 
 public class Move_Zeroes_283 {
 
+    //Solution 1
     public void moveZeroes(int[] nums) {
+        int j = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                j = i;
+                break;
+            }
+        }
+
+        if (j == -1) return;
+
+        for (int i = j + 1; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[j] = nums[i];
+                nums[i] = 0;
+                j++;
+            }
+        }
+    }
+
+    //Solution 2
+    public void moveZeroes1(int[] nums) {
         int i = 0, j = 0, n = nums.length;
         while (j < n) {
             if (nums[j] != 0) {
