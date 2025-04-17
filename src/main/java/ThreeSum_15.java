4ccd940168295d9a42ba47258e25869a093fb0d6
vigneshwara palanisamy <vigneshwarap48@gmail.com>
2025-04-17 12:36:17 +0530

    15. 3Sum

--- src/main/java/ThreeSum_15.java
+++ src/main/java/ThreeSum_15.java
@@ -3,8 +3,38 @@ import java.util.Arrays;
 import java.util.List;
 
 // https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/description/
+// 15. 3Sum
 public class ThreeSum_15 {
-    public static List<List<Integer>>  threeSum(int[] nums) {
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        int n = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            if (i != 0 && nums[i] == nums[i - 1]) continue;
+
+            int j = i + 1;
+            int k = n - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum > 0) {
+                    k--;
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j-1]) j++;
+                    while(j < k && nums[k] == nums[k+1]) k--;
+                }
+            }
+        }
+
+        return ans;
+    }
+
+   /* public static List<List<Integer>>  threeSum(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
         int mainPointer = 0, left,right;
@@ -38,7 +68,7 @@ public class ThreeSum_15 {
             tmp++;
         }
         return tmp;
-    }
+    }*/
 
     public static void main(String[] args) {
         int[] nums = new int[]{-1, 0, 1, 2, -1, -4};
