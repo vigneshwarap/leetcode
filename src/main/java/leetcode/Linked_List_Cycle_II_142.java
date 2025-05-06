15b65a73febee53719c72e7b6010516e8db33a5e
vigneshwara palanisamy <vigneshwarap48@gmail.com>
2025-05-06 06:55:02 +0530

    142. Linked List Cycle II

---
+++ src/main/java/leetcode/Linked_List_Cycle_II_142.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+// 142. Linked List Cycle II
+// https://leetcode.com/problems/linked-list-cycle-ii/description/
+public class Linked_List_Cycle_II_142 {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                slow = head;
+
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
